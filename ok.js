68d161a229d349e1c8d218ed2c64544fa5a40238
root <root@wappuradio.fi>
2015-12-23 20:17:01 +0200

    backup

--- ok.js
+++ ok.js
@@ -99,6 +99,13 @@ $(function() {
             }
         }
     });
+    $('#kloonaa').click(function(e) {
+        $('#ohjelmakartta [name=id]').val('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        }));
+        $('#lisaa').submit();        
+    });
     $('#lisaa').submit(function(e) {
         e.preventDefault();
         if($('#ohjelmakartta [name=id]').val() == '') {
