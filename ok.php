1d1340c78da2bcca4f9ad2e8dec3822d9f2bcb2e
root <root@sauron.novalocal>
2017-03-24 10:17:01 +0000

    backup, blame dregu

--- ok.php
+++ ok.php
@@ -7,27 +7,31 @@ $user = $_SESSION[array_keys($_SESSION)[0]];
 if(!$user['auth']) exit;
 $name = $user['auth']['user'];
 if(!in_array('toimitus', $user['auth']['info']['grps'])) exit;
-$data = json_decode(file_get_contents('ok.json'));
+$data = json_decode(file_get_contents('ok.json'), true);
 if(!$data) $data = array();
 for($i=0;$i<sizeof($data);$i++) {
-  if($data[$i]->id == $_POST['id'] || $data[$i]->id == $_POST['delete']) {
+  if($data[$i]['id'] == $_POST['id'] || $data[$i]['id'] == $_POST['delete']) {
     unset($data[$i]);
-    $data = array_values($data);
   }
 }
+$data = array_values($data);
 if($_POST['id']) {
+  $_POST['timestamp'] = (new DateTime())->format(DateTime::ATOM);
   $data[] = $_POST;
 }
+
 $api = $data;
 for($i=0;$i<sizeof($api);$i++) {
-  $api[$i]->start = (new DateTime($api[$i]->start))->format(DateTime::ATOM);
-  $api[$i]->end = (new DateTime($api[$i]->end))->format(DateTime::ATOM);
+  $api[$i]['start'] = (new DateTime($api[$i]['start']))->format(DateTime::ATOM);
+  $api[$i]['end'] = (new DateTime($api[$i]['end']))->format(DateTime::ATOM);
+  $filename = preg_replace('/[^a-z0-9-]/', '', mb_strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $api[$i]['title']))).'-fs8.png';
+  $api[$i]['photo'] = 'https://wappuradio.fi/img/host/'.$filename;
+  $api[$i]['thumb'] = 'https://wappuradio.fi/img/host/thumb/'.$filename;
 }
 $api = json_encode($api, JSON_PRETTY_PRINT);
 $fp = fopen('api.json', 'w+');
 fwrite($fp, $api);
 fclose($fp);
-error_log(Locale::getDefault());
 
 $data = json_encode($data, JSON_PRETTY_PRINT);
 $fp = fopen('ok.json', 'w+');
